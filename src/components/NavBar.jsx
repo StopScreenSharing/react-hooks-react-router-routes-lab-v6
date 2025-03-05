5a4348873029eb45f0275324c65e28bff421d38f
aidadieudonne <aidaharing10@gmail.com>
2025-03-04 17:13:14 -0700

    learning routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,19 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
+      <NavLink to="/" className="nav-bar">
+        Home
+      </NavLink>
+      
+      <NavLink to="/directors" className="nav-bar">
+        Directors
+      </NavLink>
+     
+      <NavLink to="/actors" className="nav-bar">
+        Actors
+      </NavLink>
+    
+        
     
     </nav>
     );
